a115bf1c1afd07b9435d1eed30f8bffbc5923e18
Will Piwowarski <williamtpiwo@gmail.com>
2026-05-20 19:03:35 -0400

    Switch database provider to Postgres

--- prisma/migrations/20260517233703_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Dataset" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "fileName" TEXT NOT NULL,
-    "rowCount" INTEGER NOT NULL,
-    "columnCount" INTEGER NOT NULL,
-    "uploadedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
--- prisma/migrations/20260518152445_add_file_path_to_dataset/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Dataset" ADD COLUMN "filePath" TEXT;
